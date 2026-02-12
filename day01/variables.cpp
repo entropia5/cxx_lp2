906d66d8560f8b8e46702df14fb09067d2769c80
igor y <yakobchuk.igor@yahoo.com>
2026-02-12 17:04:36 +0200

    add variables exercise

--- day01/variables.cpp
+++ day01/variables.cpp
@@ -1,12 +1,19 @@
-// variables.cpp by Bill Weinman [bw.org]
-// updated 2022-06-12
+// 3
+
+
 #include <format>
 #include <iostream>
+
 
 using std::format;
 using std::cout;
 
+
 int main() {
     int i {};
-    cout << format("i is {}\n", i);
+    cout << format("i is {}\n", i); //здесь в потоке вывода результат: i = 0;
+    i = 10; //присвоим значение переменной i;
+    cout << format("i is {}\n", i); // вывод здесь покажет, что i = 10;
+
+return 0;
 }

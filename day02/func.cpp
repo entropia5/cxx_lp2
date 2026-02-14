3db941c4f5570612bdb629fba42bc1277e309c12
igor y <yakobchuk.igor@yahoo.com>
2026-02-14 17:21:10 +0200

    add cpp file

---
+++ day02/func.cpp
@@ -0,0 +1,20 @@
+//day2, ex4
+
+
+
+#include <format>
+#include <iostream>
+
+using std::format;
+using std::cout;
+
+void func() {
+     cout << "this is func()\n";
+}
+
+int main()
+{
+    cout << "this is main()\n";
+    func(); //вызов ф-ции
+}
+

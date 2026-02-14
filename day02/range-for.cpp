e2a08938c03f414bc37b1a29f59330d3edab02c2
igor y <yakobchuk.igor@yahoo.com>
2026-02-14 12:56:24 +0200

    add range-for.cpp

--- day02/range-for.cpp
+++ day02/range-for.cpp
@@ -1,5 +1,6 @@
-// range-for.cpp by Bill Weinman [bw.org]
-// updated 2022-05-23
+//продолжениие для for
+
+
 #include <format>
 #include <iostream>
 
@@ -9,7 +10,7 @@ using std::cout;
 int main() {
     int array[] { 1, 2, 3, 4, 5 };
 
-    for (const auto& e : array) {
+    for (const auto& e : array) {   //элемент который получаем в левой части цикла for является ссылкой на фактический элемент в массиве
         cout << format("element is {}\n", e);
     }
 }

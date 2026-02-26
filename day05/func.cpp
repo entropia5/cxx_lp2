7eb07fc07a64c411ca413afd3a18f0b59b61dca6
igor y <yakobchuk.igor@yahoo.com>
2026-02-26 20:37:58 +0200

    update ex1

--- day05/func.cpp
+++ day05/func.cpp
@@ -1,7 +1,7 @@
 //day05
 
 
-
+/* 
 #include <format>
 #include <iostream>
 
@@ -16,3 +16,59 @@ int main() {
     cout << "this is main()\n";
     func();
 }
+
+
+*/
+
+
+
+#include <iostream>
+#include <format>
+
+using std::format;
+using std::cout;
+
+
+void Inc1 (int value)
+{
+    value ++;
+    
+}
+
+
+void Inc2 (int& value)
+{
+    value ++;
+    
+}
+
+
+void Inc3 (const int& value)
+{
+    
+}
+int main ()
+{
+    int a = 5;
+    
+    Inc1(a);
+    cout << format("Inc1 value is a {}\n", a);
+    
+    Inc2(a);
+    cout << format("Inc2 value is a {}\n", a);
+    
+    Inc3(a);
+    cout << format("Inc3 value is a {}\n", a);
+    
+    
+    
+    
+    
+    
+    return 0;
+}
+
+
+
+
+

b83ad7eb42ae0a8737f5c9f0038ab28d5eb4917f
igor y <yakobchuk.igor@yahoo.com>
2026-03-07 16:37:59 +0200

    add ex9 sqrt

---
+++ Back_to_the_basics/ex9.cpp
@@ -0,0 +1,13 @@
+// Дано вещественное число, вывести квадратный корень этого числа.
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void Sqrt()
+{
+    double x;
+    cin >> x;
+
+    cout << sqrt(x) << endl;
+}

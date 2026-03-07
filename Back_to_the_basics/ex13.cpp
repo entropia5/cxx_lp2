6af6e37bf26e9cafdf1a3843ecf07b0f49b72aeb
igor y <yakobchuk.igor@yahoo.com>
2026-03-07 20:30:47 +0200

    add ex13 найти min двух чисел

---
+++ Back_to_the_basics/ex13.cpp
@@ -0,0 +1,15 @@
+// Даны два целых числа, найти минимум.
+
+#include <iostream>
+using namespace std;
+
+void mainx()
+{
+    int a, b;
+
+    cin >> a >> b;
+    if (a < b)
+        cout << a;
+    else
+        cout << b;
+}

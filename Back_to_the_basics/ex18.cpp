816a0bb784ca1d11ca0f6cc4a209b6fd8ba5dd20
igor y <yakobchuk.igor@yahoo.com>
2026-03-09 13:41:35 +0200

    add ex18

---
+++ Back_to_the_basics/ex18.cpp
@@ -0,0 +1,31 @@
+/*
+Дано целое число.
+Если число положительное, вывести:
+
+YES
+
+Если число отрицательное, вывести:
+NO
+
+
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "INPUT NUM: " << std::endl;
+    int a;
+
+    std::cin >> a;
+    if (a > 0)
+    {
+        std::cout << "YES" << std::endl;
+    }
+    else if (a < 0)
+    {
+        std::cout << "NO" << std::endl;
+    }
+
+    return 0;
+}

085c78d0d5d9437617acbe57b8b39a016cad9feb
igor y <yakobchuk.igor@yahoo.com>
2026-03-12 22:26:52 +0200

    add ex56

---
+++ Back_to_the_basics/ex56.cpp
@@ -0,0 +1,15 @@
+// Структуры
+
+/*
+Создать структуру Person
+Создайте структуру Person с полями name строчного типа и age целочисленного типа.
+ */
+#pragma once
+#include <string>
+using namespace std;
+
+struct Person
+{
+    string name;
+    int age;
+};

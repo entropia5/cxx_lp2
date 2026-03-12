a4c5b7a5a5b04da2c5698a3b4a202b4913324e98
igor y <yakobchuk.igor@yahoo.com>
2026-03-12 18:27:49 +0200

    add ex51 functions_basics

---
+++ Back_to_the_basics/ex51.cpp
@@ -0,0 +1,26 @@
+/*
+Создать функцию
+Напишите функцию getHello() которая возвращает строку "Hello"
+
+#include <string>
+using namespace std;
+
+string getHello() {
+    return "Hello";
+}
+
+
+*/
+/*
+Создать функцию 2
+Напишите функцию myfunction() которая выводит на экран строку "My function" и возвращает число 0.
+*/
+
+#include <iostream>
+using namespace std;
+
+int myfunction()
+{
+    cout << "My function" << endl;
+    return 0;
+}

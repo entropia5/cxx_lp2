e5d217314f5eadc817f47336f0ccbd7d2a33c678
igor y <yakobchuk.igor@yahoo.com>
2026-03-16 14:39:16 +0200

    virtual_method2

--- Object_Oriented_Programming_OOP/virtual_method.cpp
+++ Object_Oriented_Programming_OOP/virtual_method.cpp
@@ -1,6 +1,6 @@
 /*Абстрактный Shape
 Нужно переделать класс Shape в абстрактный. 
-Методы calculateArea и calculatePerimeter переделываем как чисто виртуальные.*/
+Методы calculateArea и calculatePerimeter переделываем как чисто виртуальные.
 
 
 #pragma once
@@ -49,6 +49,60 @@ private:
     
 public:
     Rectangle(string c, int a, int b) : Shape(c) {
+        this->a = a;
+        this->b = b;
+    }
+    
+    int calculateArea() override {
+        return a * b;
+    }
+    
+    int calculatePerimeter() override {
+        return (a + b) * 2;
+    }
+};
+
+
+*/
+
+/*Интерфейс Shape
+Нужно переделать класс Shape в интерфейс. Убираем поле color, метод info и конструктор у класса Shape.*/
+
+#pragma once
+#include <string>
+using namespace std;
+ 
+class Shape {
+public:
+    virtual int calculateArea() = 0;
+    virtual int calculatePerimeter() = 0;
+};
+ 
+class Circle : public Shape {
+private:
+    int radius;
+    
+public:
+    Circle(int r) {
+        radius = r;
+    }
+    
+    int calculateArea() override {
+        return 3.14 * radius * radius;
+    }
+    
+    int calculatePerimeter() override {
+        return 2 * 3.14 * radius;
+    }
+};
+ 
+class Rectangle : public Shape {
+private:
+    int a;
+    int b;
+    
+public:
+    Rectangle(int a, int b) {
         this->a = a;
         this->b = b;
     }

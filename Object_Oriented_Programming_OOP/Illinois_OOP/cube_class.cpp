e9c97061e61ffd8d03a5a890a7106ac7b32521aa
igor y <yakobchuk.igor@yahoo.com>
2026-03-22 21:28:23 +0200

    class Cube_

--- Object_Oriented_Programming_OOP/Illinois_OOP/cube_class.cpp
+++ Object_Oriented_Programming_OOP/Illinois_OOP/cube_class.cpp
@@ -1,16 +1,16 @@
 #include "Cube.h"
 
 double Cube::getVolume() {
-    return lenght_ * lenght_ * lenght_;
+    return length_ * length_ * length_;
 
 }
 
 double Cube::getSurfaceArea() {
-    return 6 * lenght_ * lenght_;
+    return 6 * length_ * length_;
 }
 
-void Cube::setLenght(double lenght) {
-    lenght_ = lenght;
+void Cube::setLength(double length) {
+    length_ = length;
 
 }
 
